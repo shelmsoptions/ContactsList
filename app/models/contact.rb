43fb9bb68c4b150949506ba15e8cf6c238bc951a
Scott Helms <shelms@imageoptions.net>
2018-01-12 17:07:06 -0800

    CL-005 default_scope order set for DeptTitle Contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,5 @@
 class Contact < ApplicationRecord
   belongs_to :dept_title
+  
+  default_scope { order('lower(first_name) asc') }
 end
--- app/models/dept_title.rb
+++ app/models/dept_title.rb
@@ -1,3 +1,5 @@
 class DeptTitle < ApplicationRecord
   has_many :contacts
+  
+  default_scope { order('lower(name) asc') }
 end
